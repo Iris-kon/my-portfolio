5c36b0cd81ea01af82b27ded51f81686f3039e39
Thiago Oliveira <thiagooliveira.k.on@gmail.com>
2023-01-29 05:00:43 -0300

    creat work display

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -7,8 +7,8 @@ export function Skills() {
     <div id="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
-          <p className="text-4xl font-bold border-b-4 border-pink-600 py-4 w-24">Skills</p>
-          <p className="py-4">&#47;&#47; These are the technologies I&rsquo;ve worked with</p>
+          <p className="text-4xl font-bold border-b-4 border-pink-600 inline">Skills</p>
+          <p className="py-6">&#47;&#47; These are the technologies I&rsquo;ve worked with</p>
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-6 gap-4 text-center py-8">
