e9479a5bc763e21dd5e1e5f38e7cb24f80d3f5c4
Thiago Oliveira <thiagooliveira.k.on@gmail.com>
2023-01-29 02:25:31 -0300

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NavbAr } from './components/Navbar'
+
+function App() {
+  return (
+    <div>
+      <NavbAr />
+      <h1 className="text-3xl font-bold underline">Hello</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function NavbAr() {
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]"></div>
+  )
+}
